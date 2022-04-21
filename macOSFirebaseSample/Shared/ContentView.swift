34175110ddb1e3e8dc282567124f8e17c3ed0ad3
ryoya ito <ry.itto.github@gmail.com>
2022-04-21 13:41:00 +0900

    Init firebase

---
+++ macOSFirebaseSample/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 伊藤凌也 on 2022/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOSFirebaseSample/Shared/macOSFirebaseSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOSFirebaseSampleApp.swift
+//  Shared
+//
+//  Created by 伊藤凌也 on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct macOSFirebaseSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
